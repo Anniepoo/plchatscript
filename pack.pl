3a944088e22704e805bb516ba5941dd3819334f3
Anniepoo <annie66us@yahoo.com>
2015-05-01 18:33:04 -0700

    updated download setting, trying to make a valid pack

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(plchatscript).
 title('Interface to the ChatScript server.').
-version('1.0.0').
+version('1.0.1').
 author('Anne Ogborn', 'annie@swi-prolog.org').
 maintainer('Anne Ogborn', 'annie@swi-prolog.org').
 packager('Anne Ogborn', 'annie@swi-prolog.org').
 home('https://github.com/Anniepoo/plchatscript').
-download('https://github.com/Anniepoo/plchatscript.git').
+download('https://github.com/Anniepoo/plchatscript/archive/v1.0.1.zip').
 provides(plchatscript).
 
