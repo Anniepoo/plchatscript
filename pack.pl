fb60309362b25cc0f77c9b1c18b40fa331438303
Anniepoo <annie66us@yahoo.com>
2015-05-01 19:00:41 -0700

    v1.0.4 release using private zip

--- pack.pl
+++ pack.pl
@@ -1,9 +1,10 @@
 name(plchatscript).
 title('Interface to the ChatScript server.').
-version('1.0.3').
+version('1.0.4').
 author('Anne Ogborn', 'annie@swi-prolog.org').
 maintainer('Anne Ogborn', 'annie@swi-prolog.org').
 packager('Anne Ogborn', 'annie@swi-prolog.org').
 home('https://github.com/Anniepoo/plchatscript').
+download('http://www.pathwayslms.com/packs/plchatscript-*.zip').
 provides(plchatscript).
 
