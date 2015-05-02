adf06c7f017112a316eed0c34fdb050be60b5014
Anniepoo <annie66us@yahoo.com>
2015-05-01 18:53:16 -0700

    more struggle

--- pack.pl
+++ pack.pl
@@ -1,10 +1,9 @@
 name(plchatscript).
 title('Interface to the ChatScript server.').
-version('1.0.2').
+version('1.0.3').
 author('Anne Ogborn', 'annie@swi-prolog.org').
 maintainer('Anne Ogborn', 'annie@swi-prolog.org').
 packager('Anne Ogborn', 'annie@swi-prolog.org').
 home('https://github.com/Anniepoo/plchatscript').
-download('https://github.com/Anniepoo/plchatscript/release/*.zip').
 provides(plchatscript).
 
