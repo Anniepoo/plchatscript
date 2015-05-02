d8508ff5f126b320fbda682ffbda4b07381a868a
Anniepoo <annie66us@yahoo.com>
2015-05-01 18:45:47 -0700

    v1.0.2 with * in download

--- pack.pl
+++ pack.pl
@@ -1,10 +1,10 @@
 name(plchatscript).
 title('Interface to the ChatScript server.').
-version('1.0.1').
+version('1.0.2').
 author('Anne Ogborn', 'annie@swi-prolog.org').
 maintainer('Anne Ogborn', 'annie@swi-prolog.org').
 packager('Anne Ogborn', 'annie@swi-prolog.org').
 home('https://github.com/Anniepoo/plchatscript').
-download('https://github.com/Anniepoo/plchatscript/archive/v1.0.1.zip').
+download('https://github.com/Anniepoo/plchatscript/release/*.zip').
 provides(plchatscript).
 
